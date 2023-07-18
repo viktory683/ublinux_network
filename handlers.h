f5fa4d9ad3d8087538d73c4b29920f7e7d2546e6
bzglve <viktory683@gmail.com>
2023-07-18 16:02:03 +0600

    added base IP handler and parser

--- handlers.h
+++ handlers.h
@@ -1,7 +1,11 @@
 #include <gtk/gtk.h>
 #include <stdio.h>
 
+/**
+ * @todo update all UI based on device info
+*/
 void deviceBoxHandler(GtkComboBoxText* box, gpointer user_data) {
     char* choosen = gtk_combo_box_text_get_active_text(box);
     printf("choosen device: '%s'\n", choosen);
 }
+
--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "handlers.h"
+#include <arpa/inet.h>
 #include <gtk/gtk.h>
 #include <jansson.h>
 #include <stdbool.h>
@@ -9,7 +10,12 @@
 
 GtkBuilder* builder;
 GtkWindow* topWindow;
+
 GtkComboBoxText* deviceBox;
+
+GtkEntry* ipAddress;
+GtkPopover* ipAddressErrorPopup;
+GtkLabel* ipAddressErrorText;
 
 void updateComboBoxItems(GtkComboBoxText* box, int itemc, char** items) {
     for (int i = 0; i < itemc; i++) {
@@ -54,7 +60,7 @@ void executeCommand(const char* command, char** output) {
 
     fp = popen(command, "r");
     if (fp == NULL) {
-        printf("Failed to execute command\n");
+        fprintf(stderr, "Failed to execute command\n");
         return;
     }
 
@@ -65,7 +71,7 @@ void executeCommand(const char* command, char** output) {
 
         char* newOutput = realloc(*output, outputSize + lineSize + 1);
         if (newOutput == NULL) {
-            printf("Failed to allocate memory\n");
+            fprintf(stderr, "Failed to allocate memory\n");
             free(*output);
             *output = NULL;
             break;
@@ -96,7 +102,7 @@ json_t* parse_json(const char* text) {
     return root;
 }
 
-void get_devices(json_t* root, char*** devices, int* devices_count) {
+void parse_devices(json_t* root, char*** devices, int* devices_count) {
     if (!json_is_array(root)) {
         fprintf(stderr, "error: root is not an array\n");
         json_decref(root);
@@ -107,9 +113,9 @@ void get_devices(json_t* root, char*** devices, int* devices_count) {
     *devices_count = (int)json_array_size(root);
 
     for (int i = 0; i < json_array_size(root); i++) {
-        json_t *data,
-            *device; //, *type, *hwaddr, *mtu, *state, *state_text, *connection, *con_path, *ip4_address_1,
-                     //*ip4_gateway, *ip4_route_1, *ip4_route_2, *ip4_dns_1, *ip6_address_1, *ip6_gateway, *ip6_route_1;
+        json_t *data, *device;
+        //, *type, *hwaddr, *mtu, *state, *state_text, *connection, *con_path, *ip4_address_1, *ip4_gateway,
+        //*ip4_route_1, *ip4_route_2, *ip4_dns_1, *ip6_address_1, *ip6_gateway, *ip6_route_1;
 
         data = json_array_get(root, i);
         if (!json_is_object(data)) {
@@ -129,10 +135,73 @@ void get_devices(json_t* root, char*** devices, int* devices_count) {
     }
 }
 
+void get_devices(char*** devices, int* devices_count) {
+    char* output = NULL;
+    executeCommand("nmcli device show | jc --nmcli", &output);
+
+    parse_devices(parse_json(output), devices, devices_count);
+
+    free(output);
+}
+
+char* validIpSymbols = "1234567890.";
+
+void ipAddressChanged(GtkEntry* entry, gpointer user_data) {
+    char* text = gtk_entry_get_text(entry);
+    printf("text: %s\n", text);
+
+    int ipLen = strlen(text);
+    bool wrongSymbols = false;
+
+    for (int i = 0; i < ipLen; i++) {
+        if (!strchr(validIpSymbols, text[i])) {
+            wrongSymbols = true;
+            break;
+        }
+    }
+
+    // 0  1  2  3  4  5  6  7  8  9 10 11 12 13 14
+    // 1  2  3  .  1  2  3  .  1  2  3  .  1  2  3
+    // TODO octets can be less than 100 and dump check by indexes not working
+    bool octetsPoints = false;
+    // for (int i = 3; i < ipLen; i += 4) {
+    //     if (text[i] != '.') {
+    //         octetsPoints = true;
+    //         break;
+    //     }
+    // }
+
+    if (octetsPoints || wrongSymbols) {
+        if (octetsPoints) {
+            gtk_label_set_text(ipAddressErrorText,
+                               "IP address consists of four octets separated by a dot\nExample: 192.168.0.1");
+        }
+
+        if (wrongSymbols) {
+            gtk_label_set_text(
+                ipAddressErrorText,
+                "Invalid Symbol\nIP address can contain only digits and a point ([0-9] and '.')\nExample: 192.168.0.1");
+        }
+
+        gtk_widget_show(ipAddressErrorPopup);
+        return;
+    }
+
+    struct in_addr addr;
+    addr.s_addr = inet_addr(text);
+
+    char ip_address[INET_ADDRSTRLEN];
+    if (inet_ntop(AF_INET, &(addr.s_addr), ip_address, INET_ADDRSTRLEN) == NULL) {
+        fprintf(stderr, "Invalid IP address");
+        return;
+    }
+    printf("IP: %s\n", ip_address);
+}
+
 int main(int argc, char* argv[]) {
     if (safe_check()) {
-        printf("Some commands are not found in system\n");
-        printf("Please install them and try again\n");
+        fprintf(stderr, "Some commands are not found in system\n");
+        fprintf(stderr, "Please install them and try again\n");
         return 1;
     }
 
@@ -151,26 +220,28 @@ int main(int argc, char* argv[]) {
     }
 
     topWindow = GTK_WINDOW(gtk_builder_get_object(builder, "topWindow"));
+
     deviceBox = GTK_COMBO_BOX_TEXT(gtk_builder_get_object(builder, "deviceBox"));
 
-    char* output = NULL;
-    executeCommand("nmcli device show | jc --nmcli", &output);
+    ipAddress = GTK_ENTRY(gtk_builder_get_object(builder, "ipAddress"));
+    ipAddressErrorPopup = GTK_POPOVER(gtk_builder_get_object(builder, "ipAddressPopup"));
+    ipAddressErrorText = GTK_LABEL(gtk_builder_get_object(builder, "ipAddressErrorText"));
+
+    g_signal_connect(G_OBJECT(topWindow), "destroy", gtk_main_quit, NULL);
+
+    g_signal_connect(G_OBJECT(deviceBox), "changed", deviceBoxHandler, NULL);
+
+    g_signal_connect(G_OBJECT(ipAddress), "changed", ipAddressChanged, NULL);
 
     char** devices;
     int devices_count;
-    get_devices(parse_json(output), &devices, &devices_count);
-
-    free(output);
-
+    get_devices(&devices, &devices_count);
     updateComboBoxItems(deviceBox, devices_count, devices);
 
     for (int i = 0; i < devices_count; i++) {
         free(devices[i]);
     }
     free(devices);
-
-    g_signal_connect(G_OBJECT(topWindow), "destroy", gtk_main_quit, NULL);
-    g_signal_connect(G_OBJECT(deviceBox), "changed", deviceBoxHandler, NULL);
 
     // freeing up memory
     g_object_unref(G_OBJECT(builder));
@@ -181,3 +252,6 @@ int main(int argc, char* argv[]) {
 
     return 0;
 }
+
+// TODO:
+//     * update device setting on focus-out-event from ipAddress
