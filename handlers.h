da4a1b248cbc4451558d68586a42250c9875670a
bzglve <viktory683@gmail.com>
2023-07-19 12:11:55 +0600

    moving from dump variables to structures with smart glib pointers\nGUI temporary off

--- handlers.h
+++ handlers.h
@@ -3,9 +3,8 @@
 
 /**
  * @todo update all UI based on device info
-*/
+ */
 void deviceBoxHandler(GtkComboBoxText* box, gpointer user_data) {
     char* choosen = gtk_combo_box_text_get_active_text(box);
     printf("choosen device: '%s'\n", choosen);
 }
-
